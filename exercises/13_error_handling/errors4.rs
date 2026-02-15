874713439cfbca9d073ac990e52a70fec2c2a249
Ansible2 <ansible2@proton.me>
2026-02-15 12:52:45 -0700

    Completed many exercises

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -9,9 +9,16 @@ struct PositiveNonzeroInteger(u64);
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
+        if value == 0 {
+            return Err(CreationError::Zero);
+        }
+
+        match value.is_negative() {
+            false => Ok(Self(value as u64)),
+            true => Err(CreationError::Negative),
+        }
         // TODO: This function shouldn't always return an `Ok`.
         // Read the tests below to clarify what should be returned.
-        Ok(Self(value as u64))
     }
 }
 
